fd69328b19812590e17805e68a8817a1218f952e
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-11 16:24:10 -0400

    Learned about __doc__ + added more to project 9

--- P9MediaCatalogue.py
+++ P9MediaCatalogue.py
@@ -1,6 +1,8 @@
 # Building a Media Catalogue
 
 class Movie:
+    """Parent class representing a movie."""
+    
     def __init__(self, title, year, director, duration):
         if not title.strip():
             raise ValueError("Title cannot be empty")
@@ -21,7 +23,25 @@ class Movie:
     
     def __str__(self):
         return f"{self.title} ({self.year}) - {self.duration} min, {self.director}"
-
+    
+class TVSeries(Movie):
+    """Child class representing an entire TV series."""
+    
+    def __init__(self, title, year, director, duration, seasons, total_episodes):
+        super().__init__(title, year, director, duration)
+        
+        if seasons < 1:
+            raise ValueError("Seasons must be 1 or greater")
+        
+        if total_episodes < 1:
+            raise ValueError("Total episodes must be 1 or greater")
+        
+        self.seasons = seasons
+        self.total_episodes = total_episodes
+        
+    def __str__(self):
+        return f"{self.title} ({self.year}) - {self.seasons} seasons, {self.total_episodes} episodes, {self.duration} min avg, {self.director}"
+    
 class MediaCatalogue:
     def __init__(self):
         self.items = [] # Empty List
@@ -35,9 +55,29 @@ class MediaCatalogue:
         
         result = f"Media Catalogue ({len(self.items)} items):\n\n" # <-- number of items(movies) in the catalogue
         
+        for n, movie in enumerate(self.items, 1):
+            result += f"{n}. {movie}\n"
+        return result
+
+catalogue = MediaCatalogue()
+
 try:
     movie1 = Movie("Swiss Army Man", 2016, "Daniel Scheinert and Daniel Kwan", 97)
+    catalogue.add(movie1)
+    
+    movie2 = Movie("The Matrix", 1999, "The Wachowskis", 136)
+    catalogue.add(movie2)
+    
+    series1 = TVSeries("The Flash", 2014, "Greg Berlanti, Andrew Kreisberg, and Geoff Johns", 30, 9, 184)
+    catalogue.add(series1)
+    
+    series2 = TVSeries("Scrubs", 2001, "Bill Lawrence", 24, 9, 182)
+    catalogue.add(series2)
+    # print(series1)
+    print(catalogue)
 except ValueError as e:
     print(f"Validation Error: {e}")
-    
+
+# print(movie1.__doc__)
+# print(series1.__doc__)
 # print(movie1)
